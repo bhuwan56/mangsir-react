ecd288c020d151295de1aebb03fa5967d9d8ae1c
bhuwan56 <bhatbhuwan@gmail.com>
2025-01-13 14:07:46 +0545

    Day2 React alert

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,57 @@
 import { useState } from 'react';
 import './App.css'
 import Navbar from './components/Navbar';
+import Alert from './components/Alert';
 
 function App() {
   const [count, setCount] = useState(2)
-  const [name, setName] = useState('ram')
+  const [mode, setMode] = useState('light')
+  const [text, setText] = useState('Dark mode enable')
+  const [alert, setAlert] = useState(null)
   const handleCount = () => {
     setCount( count + 1)
   }
-setTimeout(() => {
-  setName('shyam')
-}, 3000);
+  const showAlert = (type,message) => {
+    setAlert({
+      type:type,
+      message:message
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 3000);
+    
+  }
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      setText('Light mode enable')
+      showAlert('success','dark mode has been mode enable')
+    }
+    else{
+      setMode('light')
+      setText('Dark mode enable')
+      showAlert('success','light mode has been mode enable')
+    }
+  }
+
 
   return (
     <>
     
-      <Navbar title="hamro bazzar"/>
+      <Navbar 
+      title="hamro bazzar" 
+      toggleMode={toggleMode} 
+      text={text}
+       mode={mode}
+
+      
+
+       />
+        <Alert 
+        alert={alert}
+      
+      />
+    
     <div className="card">
         <button className="mt-4" onClick={handleCount}> click me!</button>
        
---
+++ src/components/Alert.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Alert = (props) => {
+
+   
+  return (
+    <div>
+        {props.alert &&(
+        <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} 
+        role="alert">
+
+  <strong>{props.alert.type}</strong>: {props.alert.message}
+  
+  </div>
+        )}
+    </div>
+  )
+}
+export default Alert
